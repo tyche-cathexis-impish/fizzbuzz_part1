774c63f77d9222dc698a2b92a057f9eb9d000378
tam <tamulamail@gmail.com>
2019-11-18 18:04:14 +0900

    refactoring, change command, output to print. こっちの方がわかりやすいと思ったから

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -3,9 +3,7 @@ number = int(input("一つの自然数を入れてね: "))
 if number % 3 == 0:
     output = "Fizz"
 elif number % 5 == 0:
-    output = "Buzz"
+    print("Buzz")
 
 else:
-    output = str(number)
-
-print(output)
+    print(str(number))
